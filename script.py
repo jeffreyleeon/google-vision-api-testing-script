b4d1405991b6d18dbb88f5ce983ec7f9e6585778
JeffreyLee <jeffreyleeon@gmail.com>
2016-02-23 22:53:54 +0800

    Curl google api and remove code for outputting temp file

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ import os
 import requests
 
 IMAGE_NAME = 'image.jpg'
-OUTPUT_FILE_NAME = 'image.json'
 # Feature types: https://cloud.google.com/vision/docs/concepts#types_of_vision_api_requests
 FEATURE_TYPE = 'LABEL_DETECTION'
 
@@ -34,11 +33,30 @@ def _create_payload_for_image(image_file):
   return payload
 
 
-def _output_payload(payload):
-  with open(OUTPUT_FILE_NAME, 'w') as f:
-    json.dump(payload, f, ensure_ascii=True)
+def _get_vision_api_url():
+  if len(GOOGLE_VISION_API_CREDENTIAL):
+    return 'https://vision.googleapis.com/v1/images:annotate?key={}'.format(GOOGLE_VISION_API_CREDENTIAL)
+  else:
+    return ''
+
+
+def _analyse_image(payload):
+  headers = {
+    'content-type': 'application/json'
+  }
+  url = _get_vision_api_url()
+  
+  data = json.dumps(payload)
+  res = requests.post(
+    url=url,
+    data=data,
+    headers=headers
+    )
+  return res.text
 
 
 with open(IMAGE_NAME, 'rb') as image_file:
-    payload = _create_payload_for_image(image_file)
-    _output_payload(payload)
+  payload = _create_payload_for_image(image_file)
+  result = _analyse_image(payload)
+  print(result)
+
